0ebf7cc6ee2efbade9c1fb56c49514e7d78a0caf
grayTurtle01 <the.correo@protonmail.com>
2022-03-22 03:26:37 -0600

    update: render cards with v-for

--- src/components/MenuSecction.vue
+++ src/components/MenuSecction.vue
@@ -4,13 +4,11 @@
 
         <div class="contenedor">
 
-            <Card title="Tres Chocolates" price="435.00" image="tres-chocolates.jpg"/>
-            
-            <Card title="Red Velvet" price="400.00" image="red-velvet.jpg"/>
-
-            <Card title="Zanahoria" price="395.00" image="zanahoria.jpg"/>
-
-            <Card title="Mil Hojas" price="299.00" image="mil-hojas.png"/>
+			<Card v-for="flavor in $store.state.flavors"
+				v-bind:title="flavor.title"
+				v-bind:price="flavor.price"
+				v-bind:image="flavor.image"
+			/>
 
         </div>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,31 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    x: 42,
+
+    flavors: [
+      {
+        title: 'Tres Chocolates',
+        price: 435,
+        image: 'tres-chocolates.jpg'
+      },
+      {
+        title: 'Red Velvet',
+        price: 400,
+        image: 'red-velvet.jpg'
+      },
+      {
+        title: 'Zanahoria',
+        price: 395,
+        image: 'zanahoria.jpg'
+      },
+      {
+        title: 'Mil Hojas',
+        price: 299,
+        image: 'mil-hojas.png'
+      }
+
+    ]
   },
   getters: {
   },
